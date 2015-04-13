b9621e1b41bc5b2bb9cb112db3c97d51146a7a7e
Star <xingyi7@126.com>
2015-04-13 13:17:25 +0800

    基本实现天气预报功能，界面及功能设计有待优化，多日预报用到了多线程，第一次打开不一定能显示出来，天气预报的图标暂时还不能改变

--- app/src/main/java/com/coolweather/app/activity/WeatherShowActivity.java
+++ app/src/main/java/com/coolweather/app/activity/WeatherShowActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -20,6 +21,8 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,8 +37,17 @@ public class WeatherShowActivity extends Activity {
             + "getWeather?theUserID=&theCityCode=";
 
     private List<Weather> weatherList = new ArrayList<Weather>();
+    private TextView city_name_view;
+    private TextView today_date_view;
+    private TextView today_sunny_view;
+    private ImageView today_image_view;
+    private TextView today_tempreature_view;
+    private TextView today_wind_view;
+    private TextView today_air_view;
+    private TextView today_ultraviolet_view;
+    private TextView today_humidity_view;
     List<String> allWeatherInfo = new ArrayList<String>();
-    private City city;
+    private City city = new City();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,22 +65,61 @@ public class WeatherShowActivity extends Activity {
         getAllWeatherInfo();
 
         WeatherAdapterUtil adapter = new WeatherAdapterUtil(WeatherShowActivity.this,R.layout.weather_item,weatherList);
-        ListView listView = (ListView)findViewById(R.id.list_view);
+        ListView listView = (ListView)findViewById(R.id.weather_list);
         listView.setAdapter(adapter);
+
+
     }
 
     public void getAllWeatherInfo(){
         if(allWeatherInfo.size() > 0){
-            Weather weather = new Weather();
+            String temp;
+            String[] tempArray;
+            city_name_view = (TextView)findViewById(R.id.city_name);
+            today_date_view = (TextView)findViewById(R.id.today_date);
+            today_sunny_view = (TextView)findViewById(R.id.today_sunny);
+            today_image_view = (ImageView)findViewById(R.id.today_image);
+            today_tempreature_view = (TextView)findViewById(R.id.today_temperature);
+            today_wind_view = (TextView)findViewById(R.id.today_wind);
+            today_air_view = (TextView)findViewById(R.id.today_air);
+            today_ultraviolet_view = (TextView)findViewById(R.id.today_ultraviolet);
+            today_humidity_view = (TextView)findViewById(R.id.today_humidity);
+
+            city_name_view.setText(allWeatherInfo.get(1));
+            temp = allWeatherInfo.get(3);
+            tempArray = temp.split(" ");
+            today_date_view.setText(tempArray[0]);
+            temp = allWeatherInfo.get(7);
+            tempArray = temp.split(" ");
+            today_sunny_view.setText(tempArray[1]);
+            temp = allWeatherInfo.get(5);
+            tempArray = temp.split("；");
+            today_air_view.setText(tempArray[0]);
+            today_ultraviolet_view.setText(tempArray[1]);
+            temp = allWeatherInfo.get(4);
+            tempArray = temp.split("；");
+            String[] tempArray2;
+            tempArray2 = tempArray[0].split("：");
+            today_tempreature_view.setText(tempArray2[2]);
+            tempArray2 = tempArray[1].split("：");
+            today_wind_view.setText(tempArray2[1]);
+            today_humidity_view.setText(tempArray[2]);
+            today_image_view.setImageResource(R.drawable.a_11);
+
+
+
+
             for(int i = 0; i < 5; i++){
-                String temp = allWeatherInfo.get(7+5*i);
-                String[] tempArray = temp.split(" ");
+                Weather weather = new Weather();
+                temp = allWeatherInfo.get(7+5*i);
+                tempArray = temp.split(" ");
                 weather.setDate(tempArray[0]);
                 weather.setSunny(tempArray[1]);
                 weather.setTemperature(allWeatherInfo.get(8+5*i));
                 weather.setWind(allWeatherInfo.get(9+5*i));
                 weatherList.add(weather);
             }
+
         }else{
             getAllWeatherInfoByServer(city.getCityCode());
         }
